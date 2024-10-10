c6d38807b8450d2414e19cfd53437932da73391b
maskur <146052741+masb88625@users.noreply.github.com>
2024-10-10 22:17:10 +0800

    Add files via upload

---
+++ 03-KALKULATOR.java
@@ -0,0 +1,43 @@
+public class Main {
+    //atribut
+    String nama;
+    String NIM;
+    String prodi;
+
+    System.out.println("MASKUR");
+    System.out.println("23241065");
+    System.out.println("PTI");
+
+
+//class dengan kalkulator
+
+    # Fungsi untuk menjumlahkan dua Angka
+def penjumlahan(3, 4):
+    return 3 + 4
+
+# Fungsi untuk mengurangkan dua Angka
+def pengurangan(3, 4):
+    return 3 - 4
+
+# Fungsi untuk mengalikan dua Angka
+def perkalian(3, 4):
+    return 3 * 4
+
+# Fungsi untuk membagi dua Angka
+def pembagian(3, 4):
+    if b == 0:
+        return "Error: Pembagian dengan nol tidak diperbolehkan"
+    return 3 / 4
+
+# Input dari pengguna
+angka1 = float(input("Masukkan Angka pertama: "))
+angka2 = float(input("Masukkan Angka kedua: "))
+
+# Menampilkan hasil
+print(f"Hasil Penjumlahan: {penjumlahan(angka1, angka2)}")
+print(f"Hasil Pengurangan: {pengurangan(angka1, angka2)}")
+print(f"Hasil Perkalian: {perkalian(angka1, angka2)}")
+print(f"Hasil Pembagian: {pembagian(angka1, angka2)}")
+
+    
+}
